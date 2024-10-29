12ad4f90a220529e960eeb82674c151e1cd200fe
vasilisssk <erokhovvasiliy2005@gmail.com>
2024-10-29 14:17:18 +0300

    polimorfism

--- src/ru/vsu/cs/erokhov_v_e/task_1/Main.java
+++ src/ru/vsu/cs/erokhov_v_e/task_1/Main.java
@@ -1,5 +1,8 @@
 package ru.vsu.cs.erokhov_v_e.task_1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -22,31 +25,39 @@ public class Main {
         magic.addBooks(GCaPoG, GCaPS);
         forAdults.addBooks(GK);
         discount.addBooks(WK, GCaPS);
-        System.out.println("Книги категории bestseller: " + bestseller.getBooks().toString() + " | " + bestseller.getBooksAmount());
-        System.out.println("Книги категории forKids: " + forKids.getBooks().toString() + " | " + forKids.getBooksAmount());
-        System.out.println("Книги категории magic: " + magic.getBooks().toString() + " | " + magic.getBooksAmount());
-        System.out.println("Книги категории forAdults: " + forAdults.getBooks().toString() + " | " + forAdults.getBooksAmount());
-        System.out.println("Книги категории discount: " + discount.getBooks().toString() + " | " + discount.getBooksAmount());
-        System.out.print("\n");
-        System.out.println("Категории книги GCaPoG: " + GCaPoG.getCategories().toString());
-        System.out.println("Категории книги GK: " + GK.getCategories().toString());
-        System.out.println("Категории книги AHS: " + AHS.getCategories().toString());
-        System.out.println("Категории книги WK: " + WK.getCategories().toString());
-        System.out.println("Категории книги SChA: " + SChA.getCategories().toString());
-        System.out.println("Категории книги GCaPS: " + GCaPS.getCategories().toString());
-        System.out.print("\n");
+//        System.out.println("Книги категории bestseller: " + bestseller.getBooks().toString() + " | " + bestseller.getBooksAmount());
+//        System.out.println("Книги категории forKids: " + forKids.getBooks().toString() + " | " + forKids.getBooksAmount());
+//        System.out.println("Книги категории magic: " + magic.getBooks().toString() + " | " + magic.getBooksAmount());
+//        System.out.println("Книги категории forAdults: " + forAdults.getBooks().toString() + " | " + forAdults.getBooksAmount());
+//        System.out.println("Книги категории discount: " + discount.getBooks().toString() + " | " + discount.getBooksAmount());
+//        System.out.print("\n");
+//        System.out.println("Категории книги GCaPoG: " + GCaPoG.getCategories().toString());
+//        System.out.println("Категории книги GK: " + GK.getCategories().toString());
+//        System.out.println("Категории книги AHS: " + AHS.getCategories().toString());
+//        System.out.println("Категории книги WK: " + WK.getCategories().toString());
+//        System.out.println("Категории книги SChA: " + SChA.getCategories().toString());
+//        System.out.println("Категории книги GCaPS: " + GCaPS.getCategories().toString());
+//        System.out.print("\n");
 
         BookCloset bookCloset1 = new BookCloset(1);
         BookCloset bookCloset2 = new BookCloset(2);
         bookCloset1.addBooks(GCaPoG, GK, GCaPS);
         bookCloset2.addBooks(AHS, WK, SChA);
-        System.out.println(bookCloset1);
-        System.out.println(bookCloset2);
+//        System.out.println(bookCloset1);
+//        System.out.println(bookCloset2);
 
         Library library = new Library("Библиотека имени А. С. Пушкина");
-        library.addBooks(GCaPoG, GK, AHS, WK, SChA, GCaPS, GCaSmoA, AHS2);
-        library.addBooks(AHS2,  AHS2, AHS, AHS2);
-        System.out.println(library);
-        System.out.println(AHS.getCopiesNumber());
+//        library.addBooks(GCaPoG, GK, AHS, WK, SChA, GCaPS, GCaSmoA, AHS2);
+//        library.addBooks(AHS2,  AHS2, AHS, AHS2);
+//        System.out.println(library);
+//        System.out.println(AHS.getCopiesNumber());
+
+        List<BookStorage> bookStorageList = new ArrayList<>();
+        bookStorageList.add(library);
+        bookStorageList.add(new BookCloset(3));
+        for (int i = 0; i < bookStorageList.size(); i++) {
+            bookStorageList.get(i).addBooks(GCaSmoA, GK);
+            System.out.println(bookStorageList.get(i));
+        }
     }
 }
--- src/ru/vsu/cs/erokhov_v_e/task_2/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.vsu.cs.erokhov_v_e.task_2;
-
-public class Main {
-    public static void main(String[] args) {
-        Initialisation initialisation = new Initialisation(33,44);
-        System.out.println(initialisation.getNumber1());
-        System.out.println(initialisation.getNumber2());
-    }
-}
